034cde5d0557c6ae56b202e05baf9edfafcbcbd5
mpritzkoleit <maxpritzkoleit@aol.com>
2019-12-21 13:50:56 +0100

    Add EnvWrapper class. Wraps a Symbtools Environment to work with Pygent.

--- probecon/helpers/pygent_helpers.py
+++ probecon/helpers/pygent_helpers.py
@@ -4,13 +4,14 @@ class EnvWrapper(StateSpaceModel):
     """ Wrapping StateSpaceEnv environment """
 
     def __init__(self, environment):
-        self.environment = environment
-        self.uDim = environment.control_dim
-        self.xDim = environment.state_dim
+        uDim = environment.control_dim
+        ode = environment.ode
+        cost = self.eval_cost
+        x0 = environment.init_state
+        dt = environment.time_step
+        super(EnvWrapper, self).__init__(ode, cost, x0, uDim, dt)
         self.uMax = environment.control_space.high
-        super(EnvWrapper, self).__init__(self, environment.ode, self.eval_cost, environment.init_state,
-                                         environment.control_dim, environment.time_step)
-
+        self.environment = environment
 
     def eval_cost(self, state, control, new_state, t, mod):
         c = self.environment._eval_cost(state, control)
