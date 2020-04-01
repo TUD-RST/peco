747e2b8a7ea8acf1615b3305ab804219c3566381
mpritzkoleit <maxpritzkoleit@aol.com>
2020-04-01 19:06:35 +0200

    Refactor discrepancy learning.

--- probecon/control/discrepancy_learning.py
+++ probecon/control/discrepancy_learning.py
@@ -7,60 +7,119 @@ from probecon.control.trajectory_optimization import TrajectoryOptimization
 from probecon.system_models.pendulum import Pendulum
 
 class DiscrepancyLearner(object):
+    """
+    Class that implements a discrepancy model learning algorithm.
+
+    Assume the following discrete dynamics:
+
+        x[k+1] = x[k] + f(x[k], u[k]) + h(x[k], u[k])
+
+    with state 'x', control 'u', time step 'k', the dynamics model 'f' and an unknown error term 'h'.
+
+    The error term 'h', which describes the discrepancy between the system model 'f' and the real dynamics of the
+    environment is modeled by a deep ensemble.
+
+    """
     def __init__(self, real_environment, model_environment, sim_time,
-                 final_cost=None,
+                 alpha=1000.,
+                 terminal_cost=None,
                  init_opt=True,
                  render=False,
-                 second_order=False):
+                 second_order=False,
+                 sparse=False,
+                 std_max=None):
+        """
+
+        Args:
+            real_environment (probecon.system_models.StateSpaceEnv):
+                environment that represents the test bench / real environment
+            model_environment (probecon.system_models.StateSpaceEnv):
+                environment that represents the model that is used in the controller design
+            sim_time (float):
+                time horizon of the trajectory optimization
+            alpha (float):
+                parameter for the discrepancy model
+            terminal_cost (function):
+                terminal cost at the last time step
+            init_opt (bool):
+                if 'True', initial trajectory optimization is performed
+            render (bool):
+                if 'True', environements are rendered
+            second_order (bool):
+                if 'True', a sparse model (with fewer outputs) is learned
+            sparse (bool):
+                if 'True', for each output dimension a single MLP is used
+            std_max (torch.Tensor):
+                maximum values of the standard deviation
+
+        """
+        self.alpha = alpha
         self.render = render
         self.real_environment = real_environment
         self.model_environment = model_environment
         state_dim = self.model_environment.state_dim
         control_dim = self.model_environment.control_dim
+        if std_max is None:
+            std_max = torch.zeros(state_dim)
         self.deep_ensemble = StateSpaceModelDeepEnsemble(num_models=5,
-                                                    hidden_layers=[32, 32, 32],
-                                                    state_dim=state_dim,
-                                                    control_dim=control_dim,
-                                                    second_order=second_order,
-                                                    alpha=100,
-                                                    std_max=torch.zeros(state_dim))
-        self.traj_opt = TrajectoryOptimization(self.model_environment, sim_time, final_cost=final_cost)
+                                                        hidden_layers=[300, 300],
+                                                        state_dim=state_dim,
+                                                        control_dim=control_dim,
+                                                        second_order=second_order,
+                                                        sparse=sparse,
+                                                        std_max=std_max)
+
+        self.traj_opt = TrajectoryOptimization(self.model_environment, sim_time, terminal_cost=terminal_cost)
         if init_opt:
-            sol = self.traj_opt.solve()
-            self.opt_control = sol['u_sim']
-        self.model_environment.set_ode_error(self.deep_ensemble.ode)
+            print('Initial trajectory optimization')
+            self.opt_control = self._solve_ocp()
+        self.model_environment.set_ode_error(self._discrepancy_model)
         self.dataset = TransitionDataSet(state_dim, control_dim,
                                          batch_size=100,
                                          shuffle=True,
                                          type='continuous',
-                                         ode=self.model_environment.ode,
+                                         state_eq=self.model_environment.ode,
                                          second_order=second_order)
         self.sim_time = sim_time
         self.time_steps = np.arange(0., sim_time+self.model_environment.time_step, self.model_environment.time_step)
         pass
 
-    def solve_ocp(self):
+    def _solve_ocp(self):
+        """
+        Solve the optimal control problem based on the model
+
+        Returns:
+            opt_control (numpy.ndarray):
+                optimal control trajectory
+
+        """
+
         self.model_environment.reset()
-        # rollout the optimal trajectory computed for the model
-        #for control in self.opt_control:
-        #    self.model_environment.step(control)
-            #if self.render:
-            #    self.model_environment.render()
-        #self.model_environment.close()
-        # compute the optimal trajectory with the mixed model
         sol = self.traj_opt.solve()
         opt_control = sol['u_sim']
         # rollout the trajectory on the model
         self.model_environment.reset()
-        for control in opt_control:
-            self.model_environment.step(control)
-            #if self.render:
-            #    self.model_environment.render()
-        #self.model_environment.close()
+        if self.render:
+            print('Rendering optimized trajectory on the model!')
+            for control in opt_control:
+                self.model_environment.step(control)
+                self.model_environment.render()
+            self.model_environment.close()
         return opt_control
 
-    def rollout_episode(self, controls):
-        # solve trajectory optimization problem:
+    def _rollout_episode(self, controls):
+        """
+        Rollout a control trajectory on the real environment.
+
+        Args:
+            controls (numpy.ndarray):
+                control trajectory
+
+        Returns:
+            trajectory (dict):
+                trajectory dictionary with the keys: 'old_states', 'controls', 'states', 'time' and 'time_steps'
+
+        """
         for control in controls:
             self.real_environment.step(control)
             if self.render:
@@ -68,8 +127,15 @@ class DiscrepancyLearner(object):
         self.real_environment.close()
         return self.real_environment.trajectory
 
-    def rollout_random_episode(self):
-        # solve trajectory optimization problem:
+    def _rollout_random_episode(self):
+        """
+        Rollout a random control trajectory on the real environment-
+
+        Returns:
+            trajectory (dict):
+                trajectory dictionary with the keys: 'old_states', 'controls', 'states', 'time' and 'time_steps'
+
+        """
         self.real_environment.reset()
         for _ in self.time_steps:
             self.real_environment.random_step()
@@ -79,58 +145,91 @@ class DiscrepancyLearner(object):
         return self.real_environment.trajectory
 
 
-    def train_ensemble(self):
-        self.deep_ensemble.train_ensemble(self.dataset, 200, loss='nll', lr=1e-3, weight_decay=1e-5)
-        #self.deep_ensemble.train_ensemble(self.dataset, 100, loss='nll', lr=1e-4, weight_decay=1e-4)
-        #self.deep_ensemble.train_ensemble(self.dataset, 100, loss='nll', lr=1e-5, weight_decay=1e-4)
+    def _train_discrepancy_model(self,
+                                epochs=20,
+                                loss='nll',
+                                lr=1e-3,
+                                weight_decay=1e-5):
+        """
+        Train the discrepancy model.
+
+        Args:
+            epochs (int):
+                number of epochs
+            loss (str):
+                'nll': training using the negative log-likelihood
+                'mse': training using the mean-squared-error
+            lr (float):
+                learning rate for gradient descent
+            weight_decay (float):
+                weight-decay for regularization of parameters
+
+        """
+        self.deep_ensemble.train_ensemble(self.dataset, epochs=epochs, loss=loss, lr=lr, weight_decay=weight_decay)
         pass
+
+    def _discrepancy_model(self, time, state, control):
+        """
+        Discrepancy model of the form:
+            exp(-alpha*mean_var)*mean,
+        where 'mean' is the mean of the deep ensemble,
+        'mean_var' is the variance of the individual submodels mean output and captures the epistemic uncertainty and
+        alpha, which is a scalar parameter.
+
+        If the epistemic uncertainty, namely 'mean_var' is high, the term exp(-alpha*mean_var) gets smaller and
+        the 'weighted_output' approaches '0.0'. If it is low, the 'weighted_output' approaches the value of 'mean'
+
+        Args:
+            time (float):
+                time is needed by the simulator
+            state (numpy.ndarray):
+                state vector
+            control (numpy.ndarray):
+                control input vector
+
+        Returns:
+            weighted_output (numpy.ndarray):
+                weighted output as described above
+        """
+        mean, variance, mean_var = self.deep_ensemble.state_eq(state, control)
+        weighted_output = (torch.exp(-self.alpha*mean_var)*mean).detach().numpy()[0]
+        return weighted_output
+
 
 
     def main(self, epsiodes):
+        """
+        Main loop of the algorithm
+
+        Args:
+            epsiodes (int):
+                number of episodes
+
+        """
         for epsiode in range(epsiodes):
-            print('Started episode {epsiode}!', epsiode)
+            print('Started episode {}!'.format(epsiode))
+            # 1) compute control trajectory
+            print('1) Trajectory optimization!')
             if epsiode==0:
                 controls = self.opt_control
             else:
-                controls = self.solve_ocp()
-            #trajectory = self.rollout_random_episode()
-            trajectory = self.rollout_episode(controls)
+                controls = self._solve_ocp() # solve an optimal control problem
+
+            # 2) apply the control trajectory to the system
+            print('2) Rollout!')
+            trajectory = self._rollout_episode(controls)
+
+            # 3) add the resulting trajectory to the data set
+            print('3) Data aggregation!')
             self.dataset.add_trajectory(trajectory)
-            print('Started training !')
-            self.train_ensemble()
+
+            # 4) train the discrepancy model
+            print('4) Training!')
+            self._train_discrepancy_model()
         pass
 
 
 if __name__ == '__main__':
-    """
-    def c_k(x, u):
-        x2, x1, x4, x3 = x
-        u1, = u
-        c = 1.5 * x1 ** 2 + 3 * x2 ** 2 + 0.02 * x3 ** 2 + 0.05 * x4 ** 2 + 0.05 * u1 ** 2
-        return c
-
-
-    def c_N(x):
-        x2, x1, x4, x3 = x
-        c = 100 * x1 ** 2 + 100 * x2 ** 2 + 10 * x3 ** 2 + 10 * x4 ** 2
-        return c
-
-    def ode_error(t, state, control):
-        return np.array([np.sin(state[0]), 0., 0., -0.05 * control[0]])
-
-
-    init_state = np.array([np.pi, 0, 0, 0])
-    sim_time = 3.0
-    time_step = 0.02
-
-    env = CartPole(time_step=time_step, init_state=init_state, cost_function=c_k, part_lin=True, ode_error=ode_error)
-
-    mod_env = CartPole(time_step=time_step, init_state=init_state, cost_function=c_k, part_lin=True)
-
-    learner = DiscrepancyLearner(env, mod_env, sim_time=sim_time, final_cost=c_N, render=True)
-
-    learner.main(5)
-    """
     def c_k(x, u):
         x1, x2 = x
         u1, = u
@@ -148,13 +247,13 @@ if __name__ == '__main__':
 
 
     init_state = np.array([np.pi, 0])
-    sim_time = 3.0
+    sim_time = 5.0
     time_step = 0.02
 
     env = Pendulum(time_step=time_step, init_state=init_state, cost_function=c_k, ode_error=ode_error)
 
     mod_env = Pendulum(time_step=time_step, init_state=init_state, cost_function=c_k)
 
-    learner = DiscrepancyLearner(env, mod_env, sim_time=sim_time, final_cost=c_N, render=True)
+    learner = DiscrepancyLearner(env, mod_env, sim_time=sim_time, terminal_cost=c_N, render=True)
 
     learner.main(5)
