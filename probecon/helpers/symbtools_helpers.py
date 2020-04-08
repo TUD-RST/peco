3854a453795f81d9f22c5f6d943a3b9e78edba9d
Max Pritzkoleit <max.pritzkoleit@tu-dresden.de>
2020-04-08 18:14:09 +0200

    Use partial linearization to compute A, B only when available.

--- probecon/helpers/symbtools_helpers.py
+++ probecon/helpers/symbtools_helpers.py
@@ -6,8 +6,10 @@ def create_save_model(T, V, qq, Q, R, params, file):
     # calculate state-space model and partial linearization,
     # where the input are the accelerations of the actuated state variables
     mod.calc_state_eq(simplify=False)
+    part_lin = False
     try:
         mod.calc_coll_part_lin_state_eq(simplify=False)
+        part_lin = True
     except:
         print('Collocated partial linearization could not be calculated.')
     # save model parameters
@@ -15,12 +17,13 @@ def create_save_model(T, V, qq, Q, R, params, file):
 
     # compute A and B matrix
     state_eq = mod.f + mod.g * mod.uu
-    state_eq_part_lin = mod.ff + mod.gg*mod.uu
-
     mod.ode_state_jac = state_eq.jacobian(mod.xx)
-    mod.ode_state_jac_lin = state_eq_part_lin.jacobian(mod.xx)
     mod.ode_control_jac = mod.g
-    mod.ode_control_jac_lin = mod.gg
+    if part_lin:
+        state_eq_part_lin = mod.ff + mod.gg * mod.uu
+        mod.ode_state_jac_lin = state_eq_part_lin.jacobian(mod.xx)
+        mod.ode_control_jac_lin = mod.gg
+
 
     # save model to file
     with open(file, 'wb') as open_file:
