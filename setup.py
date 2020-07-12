93471a851d78001d7eadf1c6c4e4bd38540d2530
Max Pritzkoleit <max.pritzkoleit@tu-dresden.de>
2020-07-12 13:57:29 +0200

    rename to peco

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ import setuptools
 from setuptools import setup
 
 setup(
-    name='probecon',
+    name='peco',
     version='',
     packages=setuptools.find_packages(),
     install_requires=['torch', 'gym', 'cvxopt', 'sympy', 'numpy', 'symbtools', 'matplotlib', 'ffmpeg', 'pyglet'],
     requires=['sympy_to_c (>=0.1.2)'],
-    package_data={'probecon.system_models.symbtools_model_files': ['*.p']},
-    url='https//github.com/mpritzkoleit/...',
+    package_data={'peco.system_models.symbtools_model_files': ['*.p']},
+    url='https//github.com/mpritzkoleit/peco',
     author='Max Pritzkoleit',
     author_email='Max.Pritzkoleit@tu-dresden.de',
     description = 'Python library for control and reinforcement learning',
