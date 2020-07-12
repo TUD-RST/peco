2d86d1e22f7c12a56e0575a321ff72698c1b2cea
Max Pritzkoleit <max.pritzkoleit@tu-dresden.de>
2020-07-12 13:51:32 +0200

    modfiy setup

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     name='probecon',
     version='',
     packages=setuptools.find_packages(),
-    install_requires=['torch', 'gym', 'cvxopt', 'sympy', 'numpy', 'symbtools', 'matplotlib', 'ffmpeg', 'pyglet', 'nomkl'],
+    install_requires=['torch', 'gym', 'cvxopt', 'sympy', 'numpy', 'symbtools', 'matplotlib', 'ffmpeg', 'pyglet'],
     requires=['sympy_to_c (>=0.1.2)'],
     package_data={'probecon.system_models.symbtools_model_files': ['*.p']},
     url='https//github.com/mpritzkoleit/...',
